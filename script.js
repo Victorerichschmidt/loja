be81ee9ce8409cf471f11a7e6e0e0f05966bf81c
Victorerichschmidt <159267724+Victorerichschmidt@users.noreply.github.com>
2024-05-13 12:05:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function ola() {
-    nome = prompt("digite o nome da sua mãe: ");
+    nome = prompt("digite o seu nome: ");
     alert(nome);
 }
+function toggleMenu() {
+    var menuItems = document.querySelector('.menu-items');
+    menuItems.classList.toggle('open');
+  }
