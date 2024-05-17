0e58e555dbb8c962be44ba0fe8dce694d2b6d2a8
Victorerichschmidt <159267724+Victorerichschmidt@users.noreply.github.com>
2024-05-17 07:48:11 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function ola() {
-    nome = prompt("digite o seu nome: ");
-    alert(nome);
-}
-function toggleMenu() {
-    var menuItems = document.querySelector('.menu-items');
-    menuItems.classList.toggle('open');
-  }
